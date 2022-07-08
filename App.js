851b9d86a2f69289bac1b99228dd0ad6b7847a06
한태경 <hanskd12345@gmail.com>
2022-07-08 23:07:59 +0900

    interpolation 응용

--- App.js
+++ App.js
@@ -26,15 +26,16 @@ export default function App() {
   //애니메이션은 setState에 두지말기
   // const Y = new Animated.Value(0);
 
-  const Y_POSITION = useRef(new Animated.Value(0)).current;
+  const POSITION = useRef(new Animated.ValueXY({x: 0, y: 300})).current;
   const [up, setUp] = useState(false);
 
   const toggleUp = () => setUp(prev => !prev);
   //애니메이션은 애니메이션 컴포넌트에서만 사용가능하다.
   const moveUp = () => {
-    Animated.timing(Y_POSITION, {
+    Animated.timing(POSITION, {
       toValue: up ? 300 : -300,
-      useNativeDriver: true,
+      //native에서 제공해주지 않는 애니메이션도있으니 작동안되면 아래꺼 false로하면됨
+      useNativeDriver: false,
       duration: 1000,
       // easing: Easing.bounce,
     }).start(toggleUp);
@@ -46,31 +47,35 @@ export default function App() {
     //   useNativeDriver: true,
     // }).start();
   };
-
-  const opacity = Y_POSITION.interpolate({
-    // Y_POSITION 이 -300 ,0, 300인 경우를 알고 싶다.
-    inputRange: [-300, 0, 300],
-    // 매칭하면됨 -300이면 아웃풋은1로, 0이면 0.5로 ..
-    outputRange: [1, 0.5, 1],
-    // inputRange와 outputRange 길이가 같아야함
+  const rotation = POSITION.y.interpolate({
+    inputRange: [-300, 300],
+    outputRange: ['-360deg', '360deg'],
   });
-  const borderRadius = Y_POSITION.interpolate({
+  // const opacity = Y_POSITION.interpolate({
+  //   // Y_POSITION 이 -300 ,0, 300인 경우를 알고 싶다.
+  //   inputRange: [-300, 0, 300],
+  //   // 매칭하면됨 -300이면 아웃풋은1로, 0이면 0.5로 ..
+  //   outputRange: [1, 0.5, 1],
+  //   // inputRange와 outputRange 길이가 같아야함
+  // });
+  const borderRadius = POSITION.y.interpolate({
     inputRange: [-300, 300],
     outputRange: [100, 0],
   });
-  Y_POSITION.addListener(() => {
-    console.log('Y VALUE:', Y_POSITION);
-    console.log('opacity VALUE:', opacity);
-    console.log('borderRadius VALUE:', borderRadius);
+  const bgColor = POSITION.y.interpolate({
+    inputRange: [-300, 300],
+    outputRange: ['rgb(255, 99, 71)', 'rgb(71, 166, 255)'],
   });
-  Y_POSITION.addListener(() => console.log(Y_POSITION));
-  useEffect(() => {}, []);
+
   console.log('rendering');
   return (
     <Container>
       <Pressable onPress={moveUp}>
         <AnimatedBox
-          style={{opacity, borderRadius, transform: [{translateY: Y_POSITION}]}}
+          style={{
+            backgroundColor: bgColor,
+            transform: [{rotateY: rotation}, {translateY: POSITION.y}],
+          }}
         />
       </Pressable>
     </Container>
