2af6b8767fc4961f926b8c00bca0deb9474c1d4b
한태경 <hanskd12345@gmail.com>
2022-07-08 22:47:29 +0900

    interpolation 기본

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef, useState} from 'react';
-import {Animated, Easing, TouchableOpacity} from 'react-native';
+import {Animated, Easing, Pressable, TouchableOpacity} from 'react-native';
 import styled from 'styled-components/native';
 
 const Container = styled.View`
@@ -33,9 +33,10 @@ export default function App() {
   //애니메이션은 애니메이션 컴포넌트에서만 사용가능하다.
   const moveUp = () => {
     Animated.timing(Y_POSITION, {
-      toValue: up ? 200 : -200,
+      toValue: up ? 300 : -300,
       useNativeDriver: true,
-      easing: Easing.bounce,
+      duration: 1000,
+      // easing: Easing.bounce,
     }).start(toggleUp);
     // Animated.spring(Y_POSITION, {
     //   toValue: 200,
@@ -46,14 +47,32 @@ export default function App() {
     // }).start();
   };
 
+  const opacity = Y_POSITION.interpolate({
+    // Y_POSITION 이 -300 ,0, 300인 경우를 알고 싶다.
+    inputRange: [-300, 0, 300],
+    // 매칭하면됨 -300이면 아웃풋은1로, 0이면 0.5로 ..
+    outputRange: [1, 0.5, 1],
+    // inputRange와 outputRange 길이가 같아야함
+  });
+  const borderRadius = Y_POSITION.interpolate({
+    inputRange: [-300, 300],
+    outputRange: [100, 0],
+  });
+  Y_POSITION.addListener(() => {
+    console.log('Y VALUE:', Y_POSITION);
+    console.log('opacity VALUE:', opacity);
+    console.log('borderRadius VALUE:', borderRadius);
+  });
   Y_POSITION.addListener(() => console.log(Y_POSITION));
   useEffect(() => {}, []);
   console.log('rendering');
   return (
     <Container>
-      <TouchableOpacity onPress={moveUp}>
-        <AnimatedBox style={{transform: [{translateY: Y_POSITION}]}} />
-      </TouchableOpacity>
+      <Pressable onPress={moveUp}>
+        <AnimatedBox
+          style={{opacity, borderRadius, transform: [{translateY: Y_POSITION}]}}
+        />
+      </Pressable>
     </Container>
   );
 }
