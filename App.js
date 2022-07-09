295b1748c46a71bc9076cbd69256d1ac9180cdc1
한태경 <hanskd12345@gmail.com>
2022-07-09 23:57:19 +0900

    한계점 넘을시 사라지는 카드

--- App.js
+++ App.js
@@ -30,39 +30,59 @@ const Card = styled(Animated.createAnimatedComponent(View))`
 `;
 
 export default function App() {
+  // Values
   const scale = useRef(new Animated.Value(1)).current;
   const position = useRef(new Animated.Value(0)).current;
-  const onPressIn = () =>
-    Animated.spring(scale, {toValue: 0.95, useNativeDriver: true}).start();
-  // const onPressOut = () =>
-  //   Animated.spring(scale, {
-  //     toValue: 1,
-  //     useNativeDriver: true,
-  //   }).start();
+  const rotation = position.interpolate({
+    inputRange: [-250, 250],
+    outputRange: ['-15deg', '15deg'],
+    //한계를 넘어도 작동
+    // extrapolate: 'extend',
+    // extrapolate: 'identity',
+    //시작과 끝점이 있어서 한계점이넘거가면 작동하지않음
+    // extrapolate: 'clamp',
+  });
+  // position.addListener(() =>
+  //   console.log('posiiton:', position, 'rotate', rotation),
+  // );
+  // Animations
   const onPressOut = Animated.spring(scale, {
     toValue: 1,
     useNativeDriver: true,
   });
-
+  const onPressIn = Animated.spring(scale, {
+    toValue: 0.95,
+    useNativeDriver: true,
+  });
+  const goCenter = Animated.spring(position, {
+    toValue: 0,
+    useNativeDriver: true,
+  });
   const panResponder = useRef(
     PanResponder.create({
       //터치가 감지되면 active로 전환할거냐??
       // view에서 터치를 감지할지 결정할수있도록해줌
       onStartShouldSetPanResponder: () => true,
       //기존위치에서 아래위치를 더하는듯
-      onPanResponderGrant: () => onPressIn(),
+      onPanResponderGrant: () => onPressIn.start(),
       onPanResponderMove: (_, {dx}) => {
         position.setValue(dx);
       },
-      onPanResponderRelease: () => {
-        //동시에 진행되는 애니메이션은 아래처럼
-        Animated.parallel([
-          onPressOut,
+      onPanResponderRelease: (_, {dx}) => {
+        if (dx < -320) {
+          console.log('dismiss to the left');
           Animated.spring(position, {
-            toValue: 0,
+            toValue: -500,
             useNativeDriver: true,
-          }),
-        ]).start();
+          }).start();
+        } else if (dx > 320) {
+          Animated.spring(position, {
+            toValue: 500,
+            useNativeDriver: true,
+          }).start();
+        } else {
+          Animated.parallel([onPressOut, goCenter]).start();
+        }
       },
     }),
   ).current;
@@ -72,7 +92,7 @@ export default function App() {
       <Card
         {...panResponder.panHandlers}
         style={{
-          transform: [{scale}, {translateX: position}],
+          transform: [{scale}, {translateX: position}, {rotateZ: rotation}],
         }}
       />
     </Container>
