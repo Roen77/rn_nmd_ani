bc5f54239911af5384411b316500330a6d312dae
한태경 <hanskd12345@gmail.com>
2022-07-09 22:26:29 +0900

    애니메이션 응용

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import React, {useEffect, useRef, useState} from 'react';
-import {Animated, Easing, Pressable, TouchableOpacity} from 'react-native';
+import {
+  Animated,
+  Dimensions,
+  Easing,
+  Pressable,
+  TouchableOpacity,
+} from 'react-native';
 import styled from 'styled-components/native';
+
+const {width: SCREEN_WIDTH, height: SCREEN_HEIGHT} = Dimensions.get('window');
 
 const Container = styled.View`
   flex: 1;
@@ -23,41 +31,47 @@ const Box = styled.View`
 const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
-  //애니메이션은 setState에 두지말기
-  // const Y = new Animated.Value(0);
-
-  const POSITION = useRef(new Animated.ValueXY({x: 0, y: 300})).current;
-  const [up, setUp] = useState(false);
-
-  const toggleUp = () => setUp(prev => !prev);
-  //애니메이션은 애니메이션 컴포넌트에서만 사용가능하다.
-  const moveUp = () => {
-    Animated.timing(POSITION, {
-      toValue: up ? 300 : -300,
-      //native에서 제공해주지 않는 애니메이션도있으니 작동안되면 아래꺼 false로하면됨
-      useNativeDriver: false,
-      duration: 1000,
-      // easing: Easing.bounce,
-    }).start(toggleUp);
-    // Animated.spring(Y_POSITION, {
-    //   toValue: 200,
-    //   // bounciness: 15,
-    //   tension: 50,
-    //   friction: 1,
-    //   useNativeDriver: true,
-    // }).start();
-  };
-  const rotation = POSITION.y.interpolate({
-    inputRange: [-300, 300],
-    outputRange: ['-360deg', '360deg'],
+  const POSITION = useRef(
+    new Animated.ValueXY({
+      x: -SCREEN_WIDTH / 2 + 100,
+      y: -SCREEN_HEIGHT / 2 + 100,
+    }),
+  ).current;
+  const topLeft = Animated.timing(POSITION, {
+    toValue: {
+      x: -SCREEN_WIDTH / 2 + 100,
+      y: -SCREEN_HEIGHT / 2 + 100,
+    },
+    useNativeDriver: false,
   });
-  // const opacity = Y_POSITION.interpolate({
-  //   // Y_POSITION 이 -300 ,0, 300인 경우를 알고 싶다.
-  //   inputRange: [-300, 0, 300],
-  //   // 매칭하면됨 -300이면 아웃풋은1로, 0이면 0.5로 ..
-  //   outputRange: [1, 0.5, 1],
-  //   // inputRange와 outputRange 길이가 같아야함
-  // });
+  const bottomLeft = Animated.timing(POSITION, {
+    toValue: {
+      x: -SCREEN_WIDTH / 2 + 100,
+      y: SCREEN_HEIGHT / 2 - 100,
+    },
+    useNativeDriver: false,
+  });
+  const bottomRight = Animated.timing(POSITION, {
+    toValue: {
+      x: SCREEN_WIDTH / 2 - 100,
+      y: SCREEN_HEIGHT / 2 - 100,
+    },
+    useNativeDriver: false,
+  });
+  const topRight = Animated.timing(POSITION, {
+    toValue: {
+      x: SCREEN_WIDTH / 2 - 100,
+      y: -SCREEN_HEIGHT / 2 + 100,
+    },
+    //background color는 false로 해야 먹힘
+    useNativeDriver: false,
+  });
+
+  const moveUp = () => {
+    Animated.loop(
+      Animated.sequence([bottomLeft, bottomRight, topRight, topLeft]),
+    ).start();
+  };
   const borderRadius = POSITION.y.interpolate({
     inputRange: [-300, 300],
     outputRange: [100, 0],
@@ -66,7 +80,6 @@ export default function App() {
     inputRange: [-300, 300],
     outputRange: ['rgb(255, 99, 71)', 'rgb(71, 166, 255)'],
   });
-
   console.log('rendering');
   return (
     <Container>
@@ -74,7 +87,7 @@ export default function App() {
         <AnimatedBox
           style={{
             backgroundColor: bgColor,
-            transform: [{rotateY: rotation}, {translateY: POSITION.y}],
+            transform: [...POSITION.getTranslateTransform()],
           }}
         />
       </Pressable>
