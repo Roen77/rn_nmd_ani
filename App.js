ee18bea71e39d500ebde0d93b9992edd22b3cb4c
한태경 <hanskd12345@gmail.com>
2022-07-09 23:21:01 +0900

    offset 적용

--- App.js
+++ App.js
@@ -51,6 +51,15 @@ export default function App() {
       //터치가 감지되면 active로 전환할거냐??
       // view에서 터치를 감지할지 결정할수있도록해줌
       onStartShouldSetPanResponder: () => true,
+      //기존위치에서 아래위치를 더하는듯
+      onPanResponderGrant: () => {
+        console.log('Touch Started');
+        //0에서 시작하지말고 다시 이전위치에서 시작해라
+        POSITION.setOffset({
+          x: POSITION.x._value,
+          y: POSITION.y._value,
+        });
+      },
       onPanResponderMove: (evt, {dx, dy}) => {
         POSITION.setValue({
           x: dx,
@@ -58,15 +67,18 @@ export default function App() {
         });
       },
       onPanResponderRelease: () => {
-        console.log('tocuh 끝남');
-        Animated.spring(POSITION, {
-          toValue: {
-            x: 0,
-            y: 0,
-          },
-          bounciness: 20,
-          useNativeDriver: false,
-        }).start();
+        console.log('Touch Finished');
+        // offset를 0으로 만들면서 position에 값을 념겨줌
+        POSITION.flattenOffset();
+        // console.log('tocuh 끝남');
+        // Animated.spring(POSITION, {
+        //   toValue: {
+        //     x: 0,
+        //     y: 0,
+        //   },
+        //   bounciness: 20,
+        //   useNativeDriver: false,
+        // }).start();
       },
     }),
   ).current;
